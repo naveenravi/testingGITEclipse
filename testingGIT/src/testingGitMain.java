0da6ab9b68998dac3d791fc7225b81ecaa9a93da
Naveen Ravindran <naveen27in@gmail.com>
2012-12-11 19:40:12 +1100

    added a method

--- testingGIT/src/testingGitMain.java
+++ testingGIT/src/testingGitMain.java
@@ -3,6 +3,16 @@ public class testingGitMain {
 
 	public static void main(String args[]){
 		System.out.println("Welcome.....");
-		System.out.println("hello from testing EGIT!!!");
+//		System.out.println("hello from testing EGIT!!!");
+		if(SayHello("naveen")){
+			System.out.println("Bye....");
+		}
+		
+	}
+	
+	
+	public static boolean SayHello(String name){
+		System.out.println("Hello"+name);
+		return true;
 	}
 }
